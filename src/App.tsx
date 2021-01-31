e8146b4d3bc9a821d9070996f2f347dff8095cc3
maxminoS <maxminos.1@gmail.com>
2021-01-31 08:54:19 +0700

    Implemented HTML parsing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 export const App = () => {
+  const [content, setContent] = React.useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => { setContent(e.target.value); }
+
   return (
-    <div>
-    </div>
+    <>
+      <h1 style={{ textAlign: "center" }}>Markup Parser</h1>
+      <textarea onChange={handleChange} style={{ display: "inline-block", width: "90%", height: "40vh" }} value={content}>
+      </textarea>
+      <br />
+      <div style={{ border: "1px solid black", display: "inline-block", width: "90%", height: "40vh" }} dangerouslySetInnerHTML={{ __html: content }}>
+      </div>
+    </>
   );
 }
